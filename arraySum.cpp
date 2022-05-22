5fd6571e2cdbedcc5725c0f7c2170523bd5824c1
Anushka <anushka0242.be21@chitkara.edu.in>
2022-05-22 14:50:54 +0530

    practise file

---
+++ arraySum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int i,sum=0;
+   int arr[5]={1,2,3,4,5};
+   for(i=0;i<5;i++) {
+      sum+=arr[i];
+   }
+   cout<<sum;
+   return 0;
+}
